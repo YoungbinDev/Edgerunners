e6d49bd91fdb7d64600fbddd7af84f954e208216
Youngbin Im <youngbin.dev@gmail.com>
2025-04-25 21:10:32 +0900

    하드레퍼런싱 제거
    씬에 물린 GameFeature 하드 레퍼런싱 제거

--- Assets/Scripts/Manager/GameFeatureManager.cs
+++ Assets/Scripts/Manager/GameFeatureManager.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+[System.Serializable]
+public class AssetReferenceGameFeature : AssetReferenceT<GameFeature>
+{
+    public AssetReferenceGameFeature(string guid) : base(guid)
+    {
+    }
+}
 
 public class GameFeatureManager : MonoBehaviour
 {
+    [SerializeField] 
+    private AssetReferenceGameFeature GameFeatureAssetReference;
+
+    [HideInInspector]
     public GameFeature GameFeature;
+    private AsyncOperationHandle<GameFeature> handle;
 
     public void Init()
     {
-        GameFeature.Activate();
+        if (handle.IsValid()) return;
+
+        handle = GameFeatureAssetReference.LoadAssetAsync<GameFeature>();
+        handle.WaitForCompletion();
+
+        if (handle.Status == AsyncOperationStatus.Succeeded)
+        {
+            GameFeature = handle.Result;
+            GameFeature.Activate();
+        }
+        else
+        {
+            Debug.LogError("Failed to load GameFeature.");
+        }
     }
 
     public void DeInit()
     {
-        GameFeature.Deactivate();
+        if (!handle.IsValid()) return;
+
+        if(handle.Status == AsyncOperationStatus.Succeeded)
+        {
+            GameFeature.Deactivate();
+            Addressables.Release(handle);
+        }
     }
 }
